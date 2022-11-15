0470af3e5ec5665ef8d213c62ef94e9ff7047995
bharath <v.ramakoteswararao2015@vitalum.ac.in>
2022-11-15 17:10:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import './App.css';
-import Header from './Header';
-import Task from './Task';
-import {useState, useEffect} from 'react';
-import AcceptInput from './AcceptInput';
-import { useDrop } from 'react-dnd'
-
-const getSample = () => [['Complete ten push ups', 'Due Fri Aug 8'], ['Interview scheduled for Mike', 'Due Sun Aug 10']]
-
 function App() {
   
 
